de55df9ae3bb2790fbe43a6fb37ed3ac34ec9d7c
kipnb <moonlightonetime@gmail.com>
2026-05-05 00:50:55 +0900

    r2 match

--- functions/api/user/update.js
+++ functions/api/user/update.js
@@ -24,7 +24,7 @@ export async function onRequestPost(context) {
       
       // 🚨 방금 화면에서 복사하신 진짜 주소를 아래 따옴표 안에 덮어씌워주세요!
       // (주의: 맨 뒤에 슬래시(/)가 있다면 빼고 넣어주시는게 깔끔합니다)
-      const r2PublicDomain = "https://pub-1234abcd5678efgh.r2.dev"; 
+      const r2PublicDomain = "https://pub-ddea0ea47e83478782276137a3f52879.r2.dev"; 
       
       newImageUrl = `${r2PublicDomain}/${fileName}`; 
     }
